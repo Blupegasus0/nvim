2489dac6349ab36e6e23ce5107877c21b62cee95
Amir <kengen-bb@proton.me>
2025-02-24 00:13:45 -0400

    ok, we got gitsigns back in this mf

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -23,52 +23,52 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-    spec = {
-        -- LSP manager
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig",
+	spec = {
+		-- LSP manager
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
 
-        -- Color Schemes
-        { "wnkz/monoglow.nvim", lazy = false, priority = 1000, opts = {} },
-        { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = {} },
-        { "tanvirtin/monokai.nvim", },
+		-- Color Schemes
+		{ "wnkz/monoglow.nvim", lazy = false, priority = 1000, opts = {} },
+		{ "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = {} },
+		{ "tanvirtin/monokai.nvim", },
 
-        -- Vscode-like pictograms
-        {
-            "onsails/lspkind.nvim",
-            event = { "VimEnter" },
-        },
-        -- Auto-completion engine
-        {
-            "hrsh7th/nvim-cmp",
-            dependencies = {
-                "lspkind.nvim",
-                "hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
-                "hrsh7th/cmp-buffer", -- buffer auto-completion
-                "hrsh7th/cmp-path", -- path auto-completion
-                "hrsh7th/cmp-cmdline", -- cmdline auto-completion
-            },
-            config = function()
-                require("config.nvim-cmp")
-            end,
-        },
-        -- Code snippet engine
-        {
-            "L3MON4D3/LuaSnip",
-            version = "v2.*",
-        },
+		-- Vscode-like pictograms
+		{
+			"onsails/lspkind.nvim",
+			event = { "VimEnter" },
+		},
+		-- Auto-completion engine
+		{
+			"hrsh7th/nvim-cmp",
+			dependencies = {
+				"lspkind.nvim",
+				"hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
+				"hrsh7th/cmp-buffer", -- buffer auto-completion
+				"hrsh7th/cmp-path", -- path auto-completion
+				"hrsh7th/cmp-cmdline", -- cmdline auto-completion
+			},
+			config = function()
+				require("config.nvim-cmp")
+			end,
+		},
+		-- Code snippet engine
+		{
+			"L3MON4D3/LuaSnip",
+			version = "v2.*",
+		},
 
-        -- Fuzzy finder (file search)
-        {
-            'nvim-telescope/telescope.nvim', tag = '0.1.8',
-            dependencies = { 'nvim-lua/plenary.nvim' },
-            config = function()
-                require("config.telescope")
-            end,
-        },
+		-- Fuzzy finder (file search)
+		{
+			'nvim-telescope/telescope.nvim', tag = '0.1.8',
+			dependencies = { 'nvim-lua/plenary.nvim' },
+			config = function()
+				require("config.telescope")
+			end,
+		},
 
-        -- File Explorer
+		-- File Explorer
         {
             "nvim-tree/nvim-tree.lua",
             version = "*",
@@ -113,7 +113,8 @@ require("lazy").setup({
         },
 
         -- Tabs
-        {'romgrk/barbar.nvim',
+        {
+            'romgrk/barbar.nvim',
             dependencies = {
                 'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
                 'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
@@ -132,11 +133,17 @@ require("lazy").setup({
         },
 
         -- Git
-        { 
-            'echasnovski/mini.diff', 
-            version = false,
-            config = function() 
-                require('config.mini-diff')
+        {
+            'lewis6991/gitsigns.nvim',
+            version = "*",
+            lazy = false,
+            dependencies = {
+                'nvim-lua/plenary.nvim',
+            },
+            config = function()
+                require('gitsigns').setup {
+                    -- Other configuration options can go here
+                }
             end,
         },
 
@@ -148,6 +155,15 @@ require("lazy").setup({
             ---@type ibl.config
             opts = {},
         },
+
+        -- Auto pair (brackets and such)
+        {
+            'windwp/nvim-autopairs',
+            event = "InsertEnter",
+            config = true
+            -- use opts = {} for passing setup options
+            -- this is equivalent to setup({}) function
+        }
 
     },
     -- Configure any other settings here. See the documentation for more details.
