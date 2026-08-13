59eada2a567103b9b24ba653b24b742c436dc215
Amir Eastmond <kengen-bb@proton.me>
2026-08-13 10:29:07 -0400

    remove tmux nav plugin - nav now uses tmux natively

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -210,16 +210,6 @@ require("lazy").setup({
             end
         },
 
-        -- Tmux nav
-        {
-            "christoomey/vim-tmux-navigator",
-            config = function()
-                -- Optional: keeps it from installing helper scripts (often avoids conflicts)
-                vim.g.tmux_navigator_no_script = 1
-            end,
-        },
-
-
         ----------------------
         -- Language Support --
         ----------------------
